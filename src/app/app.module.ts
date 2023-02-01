90026d963a12fdc46132fe4b071e3cb33dc41637
Sonia Bhandari <soniabhandari19nov@gmail.com>
2023-02-01 19:24:01 +0530

    angular

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import{ReactiveFormsModule} from '@angular/forms'
+import {HttpClientModule} from '@angular/common/http';
+import { EmployeeComponent } from './employee/employee.component'
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    EmployeeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
